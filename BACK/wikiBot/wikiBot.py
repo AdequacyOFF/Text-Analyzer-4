e9596d52ba8c868ad25df1fcf9204298e9c42fec
AdequacyOFF <adequacyoff-AO@yandex.ru>
2024-05-25 16:51:57 +0300

    1

--- BACK/wikiBot/wikiBot.py
+++ BACK/wikiBot/wikiBot.py
@@ -1,6 +1,17 @@
 import requests
 class wikiBot:
-    def __init__(wikiFamily):
+    def __init__(wikiFamily="https://baza.znanierussia.ru/mediawiki/"):
+
+        tokenUrl = wikiFamily + "api.php?action=query&meta=tokens&format=json" #для получения csrf токена
+        response = requests.get(tokenUrl)
+        response_json = response.json()
+        print (response_json)
+        botToken = response_json["query"]["tokens"]["csrftoken"] #получили токен
+        print(botToken)
+
+        botLogin = "Stalnoy Voin@textAnalyzerBot" 
+        botPassword = "Andrey1Andrey"
+        url = wikiFamily+"/api.php?action=login&lgname="+botLogin+"&lgpassword="+botPassword+"&lgtoken="+botToken+"&format=json"
         response = requests.get(url)
         response_json = response.json()
         print(response_json)
